5fc7b7c76a8b535daf1f1a44ddc4ee27345f07f9
gautric <gautric@redhat.com>
2018-07-06 14:27:29 +0200

    refactor by splitting some code

---
+++ src/test/java/net/a/g/brms/dat/test/ItemUnitTestExecutor.java
@@ -0,0 +1,93 @@
+package net.a.g.brms.dat.test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.drools.core.command.runtime.SetGlobalCommand;
+import org.drools.core.command.runtime.rule.FireAllRulesCommand;
+import org.drools.core.command.runtime.rule.InsertObjectCommand;
+import org.drools.core.command.runtime.rule.QueryCommand;
+import org.drools.core.runtime.rule.impl.FlatQueryResults;
+import org.junit.jupiter.api.function.Executable;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.ExecutionResults;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.internal.command.CommandFactory;
+import org.slf4j.LoggerFactory;
+
+import net.a.g.brms.dat.model.fact.Character;
+import net.a.g.brms.dat.model.fact.Result;
+
+public class ItemUnitTestExecutor implements Executable {
+
+	private static final String RESULT = "result";
+	private static final String RESULTS = "results";
+	private StatelessKieSession kieSession;
+	private ItemUnitTestRow unitTest;
+
+	public static ItemUnitTestExecutor builder() {
+		return new ItemUnitTestExecutor();
+	}
+
+	@Override
+	public void execute() throws Throwable {
+
+		Character c = new Character();
+
+		BeanUtils.copyProperties(c, unitTest);
+
+		@SuppressWarnings("rawtypes")
+		List<Command> cmds = new ArrayList<Command>();
+		cmds.add(new SetGlobalCommand("logger",
+				LoggerFactory.getLogger(TestDroolsBatchFactory.class.getPackage().getName() + ".rule")));
+		cmds.add(new InsertObjectCommand(c));
+		cmds.add(new FireAllRulesCommand("object"));
+		cmds.add(new QueryCommand(RESULTS, "getResult"));
+
+		ExecutionResults response = kieSession.execute(CommandFactory.newBatchExecution(cmds));
+
+		FlatQueryResults queryResult = (FlatQueryResults) response.getValue(RESULTS);
+
+		Result er = (Result) queryResult.iterator().next().get(RESULT);
+
+		assertNotNull(er);
+
+		assertEquals(unitTest.isResult(), er.isOk());
+		if (unitTest.isResult()) {
+			assertEquals(unitTest.isAdult(), er.isAdult());
+		} else {
+			assertEquals(unitTest.getMessage(), er.getMessage());
+		}
+	}
+
+	public StatelessKieSession getKieSession() {
+		return kieSession;
+	}
+
+	public void setKieSession(StatelessKieSession kieSession) {
+		this.kieSession = kieSession;
+	}
+
+	public ItemUnitTestRow getUnitTest() {
+		return unitTest;
+	}
+
+	public void setUnitTest(ItemUnitTestRow unitTest) {
+		this.unitTest = unitTest;
+	}
+
+	public ItemUnitTestExecutor addKieSession(StatelessKieSession kieSession) {
+		this.kieSession = kieSession;
+		return this;
+	}
+
+	public ItemUnitTestExecutor addUnitTest(ItemUnitTestRow unitTest) {
+		this.unitTest = unitTest;
+		return this;
+	}
+
+}
--- src/test/java/net/a/g/brms/dat/test/TestDroolsBatchFactory.java
+++ src/test/java/net/a/g/brms/dat/test/TestDroolsBatchFactory.java
@@ -1,44 +1,25 @@
 package net.a.g.brms.dat.test;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.commons.beanutils.BeanUtils;
-import org.drools.core.command.runtime.DisposeCommand;
-import org.drools.core.command.runtime.SetGlobalCommand;
-import org.drools.core.command.runtime.rule.FireAllRulesCommand;
-import org.drools.core.command.runtime.rule.InsertObjectCommand;
-import org.drools.core.command.runtime.rule.QueryCommand;
-import org.drools.core.runtime.rule.impl.FlatQueryResults;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 import org.kie.api.KieServices;
-import org.kie.api.command.Command;
-import org.kie.api.runtime.ExecutionResults;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.StatelessKieSession;
-import org.kie.internal.command.CommandFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.creditdatamw.zerocell.Reader;
 
-import net.a.g.brms.dat.model.fact.Character;
-import net.a.g.brms.dat.model.fact.Result;
-
 @RunWith(JUnitPlatform.class)
 public class TestDroolsBatchFactory {
-
-	private static final String RESULT = "result";
-	private static final String RESULTS = "results";
 
 	private static Logger logger = LoggerFactory.getLogger(TestDroolsBatchFactory.class);
 
@@ -63,37 +44,9 @@ public class TestDroolsBatchFactory {
 		Collection<DynamicTest> ret = new ArrayList<>();
 		for (ItemUnitTestRow unit : listTest) {
 
-			Character c = new Character();
-
-			BeanUtils.copyProperties(c, unit);
-
 			String testName = ItemUnitTestRow.class.getName() + " [" + unit.getRowNumber() + "] = " + unit; //$NON-NLS-1$ //$NON-NLS-2$
-			DynamicTest dTest = DynamicTest.dynamicTest(testName, () -> {
-
-				@SuppressWarnings("rawtypes")
-				List<Command> cmds = new ArrayList<Command>();
-				cmds.add(new SetGlobalCommand("logger",
-						LoggerFactory.getLogger(TestDroolsBatchFactory.class.getPackage().getName() + ".rule")));
-				cmds.add(new InsertObjectCommand(c));
-				cmds.add(new FireAllRulesCommand("object"));
-				cmds.add(new QueryCommand(RESULTS, "getResult"));
-
-				ExecutionResults response = kieSession.execute(CommandFactory.newBatchExecution(cmds));
-
-				FlatQueryResults queryResult = (FlatQueryResults) response.getValue(RESULTS);
-
-				Result er = (Result) queryResult.iterator().next().get(RESULT);
-
-				assertNotNull(er);
-
-				assertEquals(unit.isResult(), er.isOk());
-				if (unit.isResult()) {
-					assertEquals(unit.isAdult(), er.isAdult());
-				} else {
-					assertEquals(unit.getMessage(), er.getMessage());
-				}
-
-			});
+			DynamicTest dTest = DynamicTest.dynamicTest(testName,
+					ItemUnitTestExecutor.builder().addKieSession(kieSession).addUnitTest(unit));
 
 			ret.add(dTest);
 		}
